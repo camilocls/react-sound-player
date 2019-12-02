9d5514d34009f625b0b38dae773148e28b63d3fe
camilocls <camiloclsx@gmail.com>
2019-12-02 08:33:03 -0500

    Add ArtWork component

---
+++ src/components/ArtWork/ArtWork.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import './ArtWork.scss'
+import artworkDefault from '../../assets/artwork.jpg'
+
+const ArtWork = (props) => {
+  const { isPlaying, artwork, isMobile } = props
+  const artworkImage = artwork ? artwork : artworkDefault
+
+  return (
+    <div className={classNames(props.className, { 'artwork--playing': isPlaying, 'artwork': !isMobile, 'artwork-mobile': isMobile })}>
+      <div
+        style={{ backgroundImage: `url(${artworkImage})` }} 
+        className={classNames({'artwork__image': !isMobile, 'artwork-mobile__image': isMobile})}
+      />
+    </div>
+  )
+}
+
+export default ArtWork
